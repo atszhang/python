94f495dd5c3e723ea7a365f020236aac0cf15362
Sophia Zhang <atszhang@gmail.com>
2024-07-23 21:52:58 -0400

    initial commit by Sophia

---
+++ ContainsDuplicate.py
@@ -0,0 +1,14 @@
+from typing import List
+class Solution:
+    def containsDuplicate(self, nums: List[int]) -> bool:
+        hashset = set()
+        for n in nums:
+            if n in hashset:
+                return True
+            hashset.add(n)
+        return False
+nums= [1,2,3,4]
+so =  Solution()
+print(so.containsDuplicate(nums))
+
+
